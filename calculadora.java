68b87f48869a9e8b1cfe9ab4e07716938b0bae2e
NataliaGit <nattorres0@gmail.com>
2017-11-08 09:42:12 -0300

    Soporte para la resta

--- calculadora.java
+++ calculadora.java
@@ -5,7 +5,14 @@ public class calculadora{
           try{
             int opt1 = Integer.parseInt(args[1]);
             int opt2 = Integer.parseInt(args[2]);
-            System.out.println(opt1 + opt2);
+            if("+".equals(args[0])){
+              System.out.println(opt1 + opt2);
+            }else if("-".equals(args[0])){
+                System.out.println(opt1 - opt2);
+            }else{
+              System.out.println(args[0] + ": operaciones desconocidas");
+            }
+
           }catch(Exception ex){
             System.out.println("Parametros incorrectos");
           }
